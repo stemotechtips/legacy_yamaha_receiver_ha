8dd392887c0b09b5eea8ef89ee40b6185e55383b
Stephen Moore <tastysmores@gmail.com>
2026-09-08 11:43:50 +1000

    Fixed a further regression - bumped version number as apparently HA doesn't like 'b' versions.

--- custom_components/legacy_yamaha_receiver_ha/__init__.py
+++ custom_components/legacy_yamaha_receiver_ha/__init__.py
@@ -19,7 +19,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     receiver_url = f"http://{entry.data['host']}/YamahaRemoteControl/ctrl"
     session = async_get_clientsession(hass)
 
-    receiver = await Receiver.initialise_receiver(session, receiver_url)
+    receiver = Receiver(session, receiver_url)
+    await receiver.initialise_receiver()
     coordinator = YamahaUpdateCoordinator(hass, entry, receiver)
     await coordinator.async_config_entry_first_refresh()
     entry.runtime_data = coordinator
